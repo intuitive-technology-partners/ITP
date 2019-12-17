cd0c3952be940625eacb537f25d433804601da23
ManikantaReddy035 <58827130+ManikantaReddy035@users.noreply.github.com>
2019-12-17 14:43:07 -0500

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,11 +1,4 @@
-resource "aws_iam_group" "mygroup" {
-  name = "mygroup"
-}
-resource "aws_iam_policy_attachment" "policyname-attach" {
-  name       = "mypolcyname-attach"
-  groups     = [aws_iam_group.mygroup.name]
-  policy_arn = "arn:aws:iam::aws:policy/policyname"
-}
+
 resource "aws_iam_user" "admin1" {
   name = "admin1", "admin2"
 }
